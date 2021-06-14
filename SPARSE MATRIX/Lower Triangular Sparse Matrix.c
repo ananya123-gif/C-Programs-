51df37ce8d98338bf2f382816b22df5dbcdca288
Ananya Shukla <65492788+ananya123-gif@users.noreply.github.com>
2021-06-14 22:30:46 +0530

    Create Lower Triangular Sparse Matrix.c

---
+++ SPARSE MATRIX/Lower Triangular Sparse Matrix.c
@@ -0,0 +1,37 @@
+/***PROGRAM FOR 1-D ARRAY IMPLEMENTATION OF LOWER TRIANGULAR SPARSE MATRIX***/
+/*****AUTHOR : ANANYA SHUKLA, ADMISSION NO. : 2019B101169********************/
+/****************************************************************************/
+
+#include<stdio.h>
+
+/****************************************************************************/
+
+void lower(int mat[3][3],int row,int col)
+{
+    int i,j,k=0,c[100];
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            if(j<=i)
+            {
+                c[k]=mat[i][j];
+                k++;
+            }
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        printf(" %d",c[i]);
+    }
+}
+
+/****************************************************************************/
+
+void main()
+{
+    int a[3][3]={1,2,3,4,5,6,7,8,9};
+    int row=3;
+    int col=3;
+    lower(a,row,col);
+}
